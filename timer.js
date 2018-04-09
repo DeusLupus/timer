d8ec8f8f95cd3cd156c5a180d8b2be0a91a53b50
Deo Bissessar <bissessardeo@gmail.com>
2018-04-08 21:35:35 -0400

    got timer started and working

--- timer.js
+++ timer.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var count = 30;
+
+var counter = setInterval(timer, 1000);
+
+function timer() {
+	count = count - 1;
+	if (count <= 0) {
+		clearInterval(counter);
+		document.getElementById("timer").innerHTML = "";
+		return;
+	}
+	document.getElementById("timer").innerHTML = count + " seconds";
+}
