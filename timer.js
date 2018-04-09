0acd4d02c11dcbe816e78f12905387bf0612563d
Deo Bissessar <bissessardeo@gmail.com>
2018-04-08 23:46:16 -0400

    nearly have the countdown working with a button

--- timer.js
+++ timer.js
@@ -2,14 +2,30 @@
 
 var count = 30;
 
-var counter = setInterval(timer, 1000);
+window.onload = pageLoad;
+
+function pageLoad() {
+	var startbtn = document.getElementById("start");
+	startbutton.onclick = timer;
 
 function timer() {
-	count = count - 1;
-	if (count <= 0) {
-		clearInterval(counter);
-		document.getElementById("timer").innerHTML = "";
-		return;
-	}
+	setInterval(function(){
+		count = count - 1;
+		if (count = 0) {
+			clearInterval(counter);
+			document.getElementById("timer").innerHTML = "";
+			return;
+		}
+	})
 	document.getElementById("timer").innerHTML = count + " seconds";
+}
+
+function stop() {
+    clearInterval(counter);
+    document.getElementById("timer").innerHTML = "";
+}
+
+function reset() {
+	count = 30;
+	document.getElementById("timer").innerHTML = "30";
 }
