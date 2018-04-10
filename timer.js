309cb128051a085a18ba5960627c289003531e82
Deo Bissessar <bissessardeo@gmail.com>
2018-04-09 22:35:11 -0400

    timer is working

--- timer.js
+++ timer.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var count = 30;
+/*
+////working countdown but on page load instead of with button
 
 var counter = setInterval(timer, 1000);
 
@@ -11,5 +13,41 @@ function timer() {
 		document.getElementById("timer").innerHTML = "";
 		return;
 	}
+	document.getElementById("timer").innerHTML = count + " seconds";
+}
+*/
+
+//attempt to have timer work with button
+window.onload = pageLoad;
+
+var counter;
+
+function pageLoad() {
+	var startbtn = document.getElementById("start");
+
+	startbtn.onclick = countdown;
+}
+
+function countdown() {
+	counter = setInterval(timer, 1000);
+}
+
+function timer() {
+	count = count - 1;
+	if (count <= 0) {
+		clearInterval(counter);
+		document.getElementById("timer").innerHTML = "";
+		return;
+	}
+	document.getElementById("timer").innerHTML = count + " seconds";
+}
+
+function stop() {
+	clearInterval(counter);
+}
+
+function reset() {
+	clearInterval(counter);
+	count = 30;
 	document.getElementById("timer").innerHTML = count + " seconds";
 }
