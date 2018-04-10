2c200cc7c2de038c3325aadcc14c6918b43f6314
Deo Bissessar <bissessardeo@gmail.com>
2018-04-09 20:58:05 -0400

    reverting to old working js

--- timer.js
+++ timer.js
@@ -2,30 +2,14 @@
 
 var count = 30;
 
-window.onload = pageLoad;
-
-function pageLoad() {
-	var startbtn = document.getElementById("start");
-	startbutton.onclick = timer;
+var counter = setInterval(timer, 1000);
 
 function timer() {
-	setInterval(function(){
-		count = count - 1;
-		if (count = 0) {
-			clearInterval(counter);
-			document.getElementById("timer").innerHTML = "";
-			return;
-		}
-	})
+	count = count - 1;
+	if (count <= 0) {
+		clearInterval(counter);
+		document.getElementById("timer").innerHTML = "";
+		return;
+	}
 	document.getElementById("timer").innerHTML = count + " seconds";
-}
-
-function stop() {
-    clearInterval(counter);
-    document.getElementById("timer").innerHTML = "";
-}
-
-function reset() {
-	count = 30;
-	document.getElementById("timer").innerHTML = "30";
 }
